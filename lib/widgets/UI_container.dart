51f2102de870931ef785c636460129a3e7de394a
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-03-13 17:53:29 +0530

    Modified - UI_container widget

--- lib/widgets/UI_container.dart
+++ lib/widgets/UI_container.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 class UIContainer extends StatelessWidget {
   final Widget children;
 
-  const UIContainer({super.key, required this.children});
+  const UIContainer(
+      {super.key,
+      required this.children,
+      this.paddingTop = 10,
+      this.paddingBottom = 10,
+      this.paddingLeft = 20,
+      this.paddingRight = 20});
+
+  final double paddingTop;
+  final double paddingBottom;
+  final double paddingLeft;
+  final double paddingRight;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             body: Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+      padding: EdgeInsets.only(
+          left: paddingLeft,
+          right: paddingRight,
+          bottom: paddingBottom,
+          top: paddingTop),
       child: children,
     )));
   }
