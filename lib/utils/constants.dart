c89ac863e93bb97f3a28d4faa40ea6d7136c157a
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-03-13 22:12:58 +0530

    Added - Constant utility file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,6 @@
-
+class Constants {
+  // space values
+  static const double smallSpace = 10.0;
+  static const double mediumSpace = 20.0;
+  static const double largeSpace = 30.0;
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,5 @@
+export './theme/theme.dart';
+export './colors.dart';
+export './constants.dart';
+export './device_utility.dart';
+export './formatter.dart';
