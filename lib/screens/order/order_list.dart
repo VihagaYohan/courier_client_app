4a272a48827d2cad78ce064066fcf2336dff7f20
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-04-13 14:28:24 +0530

    Modified - code changes in order list feature

--- lib/screens/order/order_list.dart
+++ lib/screens/order/order_list.dart
@@ -1,13 +1,20 @@
 import 'package:courier_client_app/services/service.dart';
 import 'package:courier_client_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+
+// models
+import "package:courier_client_app/models/models.dart";
 
 // widget
 import 'package:courier_client_app/widgets/widgets.dart';
 
 // providers
 import 'package:courier_client_app/provider/providers.dart';
+
+// utils
+import 'package:courier_client_app/utils/utils.dart';
 
 class OrderListScreen extends StatefulWidget {
   const OrderListScreen({super.key});
@@ -20,48 +27,51 @@ class _OrderListScreenState extends State<OrderListScreen> {
   @override
   void initState() {
     super.initState();
-/*     Provider.of<OrderProvider>(context, listen: false)
-        .getAllOrders()
-        .then((value) {
-      print(value);
-    }).catchError((e) {
-      print('Error fetching order: $e');
-    }); */
-
-    fetchOrders();
-  }
-
-  void fetchOrders() async {
-    try {
-      final response = await OrderService.getListOfOrders();
-      print(response);
-    } catch (e) {
-      print('Error occured : $e');
-    }
+    Provider.of<OrderProvider>(context, listen: false).getAllOrders();
   }
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<OrderProvider>(context);
-
-    return provider.isLoading == true
-        ? const UIProgressIndicator()
-        : UIContainer(
-            children: Center(
-            child: UITextView(text: "Order list screen"),
-          ));
-
-    /*  return Consumer<OrderProvider>(
+    return Consumer<OrderProvider>(
       builder: (context, orderProvider, child) {
         if (orderProvider.isLoading == true) {
           return const UIProgressIndicator();
+        } else if (orderProvider.isLoading == false &&
+            orderProvider.errorMessage.isNotEmpty) {
+          return Center(child: UITextView(text: orderProvider.errorMessage));
         } else {
-          return const UIContainer(
-              children: Center(
-            child: UITextView(text: 'Order list screen'),
-          ));
+          return UIContainer(
+              children: ListView.builder(
+                  itemCount: orderProvider.orderList.length,
+                  itemBuilder: (context, index) {
+                    return orderItem(context, orderProvider.orderList[index]);
+                  }));
         }
       },
-    ); */
+    );
+  }
+
+  // order item
+  Widget orderItem(BuildContext context, OrderResponse item) {
+    return GestureDetector(
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(
+            vertical: Constants.smallSpace, horizontal: Constants.mediumSpace),
+        decoration: BoxDecoration(
+            border: Border.all(color: AppColors.dark),
+            borderRadius: BorderRadius.circular(Constants.borderRadius)),
+        child: const Column(
+          children: <Widget>[
+            // tracking number
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[UITextView(text: "Tracking ID")],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
