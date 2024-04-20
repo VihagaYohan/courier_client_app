e64a9a47e6365dd13f86f9dad520e5e3dd569534
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-04-20 09:04:35 +0530

    Modified - code changes for profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,12 +1,17 @@
 import 'package:courier_client_app/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 // model
 import 'package:courier_client_app/models/models.dart';
 
 // utils
 import 'package:courier_client_app/utils/utils.dart';
-import 'package:flutter/widgets.dart';
+
+// providers
+import 'package:courier_client_app/provider/providers.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -19,7 +24,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   void initState() {
     super.initState();
-    getCurrentUser();
+    Provider.of<ProfileProvider>(context, listen: false).getCurrentUser();
   }
 
   // fetch profile data
@@ -27,7 +32,46 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const UIContainer(
-        children: Center(child: UITextView(text: "Profile screen")));
+    return Consumer<ProfileProvider>(
+      builder: (context, profileProvider, child) {
+        print(1);
+        if (profileProvider.isLoading == true) {
+          return const UIProgressIndicator();
+        } else if (profileProvider.isLoading == false &&
+            profileProvider.errorMessage.isNotEmpty) {
+          return Center(
+            child: UITextView(text: profileProvider.errorMessage),
+          );
+        } else {
+          return ListView(
+            children: <Widget>[
+              const UISpacer(
+                space: Constants.largeSpace,
+              ),
+              UITextView(text: profileProvider.currentUser.name)
+            ],
+          );
+        }
+      },
+    );
+
+    /* final provider = Provider.of<ProfileProvider>(context);
+
+    if (provider.isLoading) {
+      return const UIProgressIndicator();
+    } else if (provider.isLoading == false &&
+        provider.errorMessage.isNotEmpty) {
+      return Center(child: UITextView(text: provider.errorMessage));
+    } else {
+      return UIContainer(
+          children: ListView(
+        children: <Widget>[
+          const UISpacer(
+            space: Constants.mediumSpace,
+          ),
+          UITextView(text: "")
+        ],
+      ));
+    } */
   }
 }
