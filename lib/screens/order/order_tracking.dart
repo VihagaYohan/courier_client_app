ac655f37c03073d0aaca6cb547668cae8c16b877
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-04-21 18:10:44 +0530

    Modified - configured required packages for live tracking (Google maps and polyline_points, geo location)

--- lib/screens/order/order_tracking.dart
+++ lib/screens/order/order_tracking.dart
@@ -1,8 +1,20 @@
-import 'package:courier_client_app/widgets/widgets.dart';
+import 'dart:async';
+
+import 'package:courier_client_app/config/keys.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart' as GeoLocation;
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+// widgets
+import 'package:courier_client_app/widgets/widgets.dart';
 
 // models
 import 'package:courier_client_app/models/models.dart';
+import 'package:location/location.dart';
+
+// utils
+import 'package:courier_client_app/utils/utils.dart';
 
 class OrderTracking extends StatefulWidget {
   final OrderResponse orderDetail;
@@ -14,11 +26,141 @@ class OrderTracking extends StatefulWidget {
 }
 
 class _OrderTrackingState extends State<OrderTracking> {
+  final GeoLocation.Location locationController = GeoLocation.Location();
+
+  final Completer<GoogleMapController> controller =
+      Completer<GoogleMapController>();
+  static const CameraPosition kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 13,
+  );
+  static const mountainView = LatLng(37.3861, -122.0839);
+  LatLng? currentPosition;
+  Map<PolylineId, Polyline> polylines = {};
+
+  static const CameraPosition kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) async => {initializeMap()});
+  }
+
+  Future<void> initializeMap() async {
+    await fetchLocation();
+    final coordinates = await fetchPolylinePoints();
+    generatePolyLineFromPoints(coordinates);
+  }
+
+  Future<List<LatLng>> fetchPolylinePoints() async {
+    final polylinePoints = PolylinePoints();
+
+    final result = await polylinePoints.getRouteBetweenCoordinates(
+      KEYS.googleAPI,
+      PointLatLng(kGooglePlex.target.latitude, kGooglePlex.target.longitude),
+      PointLatLng(mountainView.latitude, mountainView.longitude),
+    );
+
+    if (result.points.isNotEmpty) {
+      return result.points
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+    } else {
+      debugPrint(result.errorMessage);
+      return [];
+    }
+  }
+
+  Future<void> generatePolyLineFromPoints(
+      List<LatLng> polylineCoordinates) async {
+    const id = PolylineId('polyline');
+
+    final polyline = Polyline(
+      polylineId: id,
+      color: AppColors.primary,
+      points: polylineCoordinates,
+      width: 5,
+    );
+
+    setState(() => polylines[id] = polyline);
+  }
+
+  Future<void> fetchLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await locationController.requestService();
+    } else {
+      return;
+    }
+
+    permissionGranted = await locationController.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await locationController.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationController.onLocationChanged.listen((currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          currentPosition =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+        print(currentPosition);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const UIContainer(
-        showAppBar: true,
-        appbar: UIAppBar(title: "Order Tracking"),
-        children: const Placeholder());
+    return UIContainer(
+      showAppBar: true,
+      appbar: const UIAppBar(title: "Order Tracking"),
+      isShowFab: false,
+      Fab: FloatingActionButton.extended(
+        onPressed: () {
+          print('google maps');
+        },
+        label: const UITextView(text: 'To the lake'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+      children: GoogleMap(
+        initialCameraPosition: kGooglePlex,
+        mapType: MapType.terrain,
+        markers: {
+          Marker(
+              markerId: const MarkerId('currentLocation'),
+              icon: BitmapDescriptor.defaultMarker,
+              position: currentPosition!),
+          Marker(
+              markerId: const MarkerId('sourceLocation'),
+              icon: BitmapDescriptor.defaultMarker,
+              position: kGooglePlex.target),
+          const Marker(
+              markerId: MarkerId("destination"),
+              icon: BitmapDescriptor.defaultMarker,
+              position: mountainView)
+        },
+        polylines: Set<Polyline>.of(polylines.values),
+        onMapCreated: (GoogleMapController mapController) {
+          controller.complete(mapController);
+        },
+      ),
+    );
   }
+
+  /* Future<void> goToLake() async {
+    final GoogleMapController controller = await controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  } */
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+import location
 import shared_preferences_foundation
 import sqflite
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
